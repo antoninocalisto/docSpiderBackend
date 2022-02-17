29e2896a0744c0971f7f3846e47e409b7b5497b7
antoninocalisto <antuninosantos@gmail.com>
2022-02-16 22:46:00 -0300

    Conexão com o banco de dados feita do back-end para o database e retornando dados

---
+++ docSpider/docSpider/Controllers/PdfController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace docSpider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PdfController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        public JsonResult Get()
+        {
+            string query = @" 
+                select * 
+                from Pdf
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSoucer = _configuration.GetConnectionString("docSpiderAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSoucer))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ docSpider/docSpider/Models/Pdf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace docSpider.Models
+{
+    public class Pdf
+    {
+        public int id { get; set; }
+
+        public string description { get; set; }
+
+        public string name_file { get; set; }
+
+        public int p_file { get; set; }
+
+        public DateTime created_at { get; set; }
+    }
+}
